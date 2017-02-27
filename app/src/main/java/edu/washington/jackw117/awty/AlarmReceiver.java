2a1c003f79a35f5659b9d5be84bb103794870e39
Jack Whitesell <jackw117@uw.edu>
2017-02-27 06:05:05 -0800

    done

--- app/src/main/java/edu/washington/jackw117/awty/AlarmReceiver.java
+++ app/src/main/java/edu/washington/jackw117/awty/AlarmReceiver.java
@@ -1,9 +1,12 @@
 package edu.washington.jackw117.awty;
 
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -15,13 +18,8 @@ public class AlarmReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         String message = intent.getStringExtra(MainActivity.MESSAGE);
         String number = intent.getStringExtra(MainActivity.NUMBER);
-        String pnumber = number;
-        if (number.length() == 10) {
-            pnumber = "(" + number.substring(0,3) + ") " + number.substring(3,6) + "-" + number.substring(6);
-        } else if (number.length() == 7) {
-            pnumber = number.substring(0, 3) + "-" + number.substring(3);
-        }
-        Toast.makeText(context, "Texting " + pnumber + ": "  + message,
-                Toast.LENGTH_SHORT).show();
+
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(number, null, message, null, null);
     }
 }
--- app/src/main/java/edu/washington/jackw117/awty/MainActivity.java
+++ app/src/main/java/edu/washington/jackw117/awty/MainActivity.java
@@ -1,12 +1,16 @@
 package edu.washington.jackw117.awty;
 
+import android.Manifest;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -31,6 +35,13 @@ public class MainActivity extends AppCompatActivity {
         final EditText message = (EditText) findViewById(R.id.editMessage);
         final EditText number = (EditText) findViewById(R.id.editNumber);
         final EditText minutes = (EditText) findViewById(R.id.editMinute);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.SEND_SMS)
+                    == PackageManager.PERMISSION_DENIED) {
+                String[] permissions = {Manifest.permission.SEND_SMS};
+                requestPermissions(permissions, 1);
+            }
+        }
         start.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
